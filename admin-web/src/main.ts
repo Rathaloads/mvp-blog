5283987a04c77a1aeef6754ec937f203c523b56d
Mr.Panda <798378281@qq.com>
2025-08-29 15:53:17 +0200

    添加pinia

--- admin-web/src/main.ts
+++ admin-web/src/main.ts
@@ -2,12 +2,16 @@ import { createApp } from 'vue';
 import "./styles/index.scss";
 import "./styles/theme.scss";
 import ElementPlus from 'element-plus'
-import { router } from "./routes/router"
-import App from './App.vue'
+import { router } from "./routes/router";
+import { createPinia } from "pinia";
+import App from './App.vue';
 
 function bootstart() {
+    const pinia = createPinia();
     const app = createApp(App);
+    
     app.use(router);
+    app.use(pinia);
     app.use(ElementPlus);
     app.mount('#app');
 }
---
+++ admin-web/src/store/global.ts
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia';
+
+export const useGlobalStore = defineStore("global", {
+    state: () => {
+        return {
+            count: 0
+        }
+    },
+
+    getters: {
+        getCount: (state) => state.count * 2,
+    },
+
+    actions: {
+        updateCount() {
+            this.count += 1;
+        }
+    }
+});
